558e622ce6d16e5aebb2c6a75c3f55db2f8855e8
Himsahu <himsahu.hs@gmail.com>
2022-06-26 23:41:15 +0530

    changes

--- Application.sh
+++ Application.sh
@@ -5,20 +5,36 @@ echo "Welcome to Secret Agent Application"
 echo "###################################"
 
 read -p "Enter your name:" name
-read -p "Enter your Favourite Actor:" actor
-read -p "Enter your Lucky Number:" lucky
-read -p "Enter your Favourite Dish:" dish
-
-first_char_name = $(echo -n $name | cut -c 1)
-len = $(echo -n $actor | wc -c)
-last_char_actor = $(echo -n $actor | cut -c $len)
-no_of_dish = $(echo -n $dish | wc -c)
-
-if [ $first_char_name = "t" -a $last_char_actor = "r" -a $no_of_dish -ge 6 -a $lucky -eq 7]
+first_char_name = $(echo -n $name | cut -c l)
+if [! $first_char_name != "d"]
 	then
-	echo "Hello Secret Agent..... Out next operation is"
-	echo "We have to kill atleast 10 sleeping students of the classroom"
-else
 	echo "Hello $name, thanks for your information"
+	exit 1
 fi
 
+read -p "Enter your Favourite Actor:" actor
+len = $(echo -n $actor | we -c)
+last_char_actor = $(echo -n $actor | cut -c $len)
+if [$last_char_actor != "r"]
+	then
+	echo "Hello $name, thanks for your information"
+	exit 1
+fi
+
+read -p "Enter your Lucky Number:" lucky
+if [$lucky -ne 7]
+	then 
+	echo "Hello $name, thanks for your information"
+	exit 1
+fi
+
+read -p "Enter your Favourite Dish:" dish
+no_of_dish = $(echo -n $dish | wc -c)
+if [ $no_of_dish -lt 6 ]
+	then
+	echo "Hello $name, thanks for your information"
+	exit 1
+fi
+
+echo "Hello Secret Agent..... Out next operation is"
+echo "We have to kill atleast 10 sleeping students of the classroom"
